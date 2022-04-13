bdcbfd0d7cdff68eb79cfc941963f13a06d4de41
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-13 01:36:54 -0300

    Update Album controller tests
    Changes:
    - Transform variables in constantsmto be used in multiple tests
    - Add Figurinha repository dependency
    - Add asserts to check association between Album and Figurinha
    - Add new test for delete method

--- src/test/java/com/zup/edu/gerenciadorde/albumdefigurinhas/controller/AlbumControllerTest.java
+++ src/test/java/com/zup/edu/gerenciadorde/albumdefigurinhas/controller/AlbumControllerTest.java
@@ -2,6 +2,9 @@ package com.zup.edu.gerenciadorde.albumdefigurinhas.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,20 +14,33 @@ import java.util.Set;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zup.edu.gerenciadorde.albumdefigurinhas.model.Album;
 import com.zup.edu.gerenciadorde.albumdefigurinhas.model.AlbumDTO;
+import com.zup.edu.gerenciadorde.albumdefigurinhas.model.Figurinha;
 import com.zup.edu.gerenciadorde.albumdefigurinhas.model.FigurinhaDTO;
 import com.zup.edu.gerenciadorde.albumdefigurinhas.repository.AlbumRepository;
+import com.zup.edu.gerenciadorde.albumdefigurinhas.repository.FigurinhaRepository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class AlbumControllerTest {
+
+    private final static String DESCRICAO = "Album de Figurinhas do campeonato Brasileiro da temporada 2021,"
+            + " com diversas estrelas venha colecionar e lembrar os melhores "
+            + "elencos deste campeonato inesquecivel, colecione as figurinhas do seu time do coração.";
+
+    private final static FigurinhaDTO FIGURINHA_DTO = new FigurinhaDTO(
+        5, "Guilherme Arana do Atletico-MG"
+    );
+
+    private final static AlbumDTO ALBUM_DTO = new AlbumDTO(
+        "Brasileirao 2021", DESCRICAO, 87, Set.of(FIGURINHA_DTO)
+    );
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -33,32 +49,53 @@ class AlbumControllerTest {
     private AlbumRepository albumRepository;
 
     @Autowired
+    private FigurinhaRepository figurinhaRepository;
+
+    @Autowired
     private MockMvc mockMvc;
 
     @Test
     void deveCadastrarUmAlbumComFigurinhas() throws Exception {
-        String descricao = "Album de Figurinhas do campeonato Brasileiro da temporada 2021,"
-                + " com diversas estrelas venha colecionar e lembrar os melhores "
-                + "elencos deste campeonato inesquecivel, colecione as figurinhas do seu time do coração.";
+        String payload = objectMapper.writeValueAsString(ALBUM_DTO);
 
-        FigurinhaDTO figurinhaDTO = new FigurinhaDTO(5, "Guilherme Arana do Atletico-MG");
-
-        AlbumDTO albumDTO = new AlbumDTO("Brasileirao 2021", descricao, 87, Set.of(figurinhaDTO));
-
-        String payload = objectMapper.writeValueAsString(albumDTO);
-
-        MockHttpServletRequestBuilder request = post("/albuns")
-                                                               .contentType(
-                                                                   MediaType.APPLICATION_JSON
-                                                               )
-                                                               .content(payload);
+        MockHttpServletRequestBuilder request = post(AlbumController.BASE_URI)
+                                                                              .contentType(
+                                                                                  APPLICATION_JSON
+                                                                              )
+                                                                              .content(payload);
 
         mockMvc.perform(request).andExpect(status().isCreated());
 
         List<Album> albums = albumRepository.findAll();
+        List<Figurinha> figurinhas = figurinhaRepository.findAll();
 
         assertFalse(albums.isEmpty());
+        assertFalse(figurinhas.isEmpty());
         assertEquals(1, albums.size());
+        assertEquals(1, figurinhas.size());
+    }
+
+    @Test
+    void deveRemoverUmAlbumComFigurinhas() throws Exception {
+        String payload = objectMapper.writeValueAsString(ALBUM_DTO);
+
+        MockHttpServletRequestBuilder request = post(AlbumController.BASE_URI)
+                                                                              .contentType(
+                                                                                  APPLICATION_JSON
+                                                                              )
+                                                                              .content(payload);
+        MockHttpServletRequestBuilder deleteRequest = delete(AlbumController.BASE_URI + "/1");
+
+        mockMvc.perform(request).andExpect(status().isCreated());
+        mockMvc.perform(deleteRequest).andExpect(status().isNoContent());
+
+        List<Album> albums = albumRepository.findAll();
+        List<Figurinha> figurinhas = figurinhaRepository.findAll();
+
+        assertTrue(albums.isEmpty());
+        assertTrue(figurinhas.isEmpty());
+        assertEquals(0, albums.size());
+        assertEquals(0, figurinhas.size());
     }
 
 }
