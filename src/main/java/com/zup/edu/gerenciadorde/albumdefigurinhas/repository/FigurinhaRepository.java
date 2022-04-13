baa1db953e34455bbf510c340138582840ff22b4
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-13 00:49:28 -0300

    Small updates
    Changes:
    - Update gitignore
    - Update configs and use YAML
    - Add exception handling classes
    - Add Figurinha repository

---
+++ src/main/java/com/zup/edu/gerenciadorde/albumdefigurinhas/repository/FigurinhaRepository.java
@@ -0,0 +1,9 @@
+package com.zup.edu.gerenciadorde.albumdefigurinhas.repository;
+
+import com.zup.edu.gerenciadorde.albumdefigurinhas.model.Figurinha;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FigurinhaRepository extends JpaRepository<Figurinha, Long> {
+
+}
